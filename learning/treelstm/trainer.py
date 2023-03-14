4f1dc6391ca55ea2d1872989ff436e02a0598bdc
izzyrybz <isabelle.ry.96@gmail.com>
2023-03-14 13:17:50 +0100

    The training is now done and showing dif pred in trash.txt

--- learning/treelstm/trainer.py
+++ learning/treelstm/trainer.py
@@ -2,6 +2,7 @@ from tqdm import tqdm
 
 import torch
 from torch.autograd import Variable as Var
+from torch.utils.data import DataLoader
 
 from learning.treelstm.utils import map_label_to_target
 
@@ -17,12 +18,14 @@ class Trainer(object):
 
     # helper function for training
     def train(self, dataset):
+        #print("we are now training")
         self.model.train()
         self.optimizer.zero_grad()
         loss, k = 0.0, 0
         indices = torch.randperm(len(dataset))
         for idx in tqdm(range(len(dataset)), desc='Training epoch ' + str(self.epoch + 1) + ''):
             ltree, lsent, rtree, rsent, label = dataset[indices[idx]]
+            #print(ltree, lsent, rtree, rsent)
             linput, rinput = Var(lsent), Var(rsent)
             target = Var(map_label_to_target(label, dataset.num_classes))
             if self.args.cuda:
@@ -39,22 +42,49 @@ class Trainer(object):
         self.epoch += 1
         return loss / len(dataset)
 
+
+
+
+
     # helper function for testing
     def test(self, dataset):
+        #for data in dataset:
+            #print("this is our data",data)
         self.model.eval()
+        
+        
+        #print(self.model)
         loss = 0
         predictions = torch.zeros(len(dataset))
         indices = torch.arange(1, dataset.num_classes + 1, dtype=torch.float)
         for idx in tqdm(range(len(dataset)), desc='Testing epoch  ' + str(self.epoch) + ''):
             ltree, lsent, rtree, rsent, label = dataset[idx]
+            #lsent, rsent,label are tensors that does have anything to do with the output
+            
+            #print("TREE",ltree.children, lsent, rtree.children, rsent, label)
+            
+
             linput, rinput = Var(lsent), Var(rsent)
+            #print("input",linput,rinput)
             target = Var(map_label_to_target(label, dataset.num_classes))
+            
             if self.args.cuda:
                 linput, rinput = linput.cuda(), rinput.cuda()
                 target = target.cuda()
+            
+            #linput = torch.tensor(x)
             output = self.model(ltree, linput, rtree, rinput)
+            
             err = self.criterion(output, target)
             loss += err.data
+            #output=output[0]
+            #print("this is output before cpu",output)
             output = output.data.squeeze().cpu()
+            #print("this is output after cpu",output)
+
+            #print("this is our output", output)
+            #print(torch.exp(output))
+            #print(indices)
             predictions[idx] = torch.dot(indices, torch.exp(output))
+            #print("this is our predications",predictions)
         return loss / len(dataset), predictions
